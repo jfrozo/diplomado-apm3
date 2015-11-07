51d53d8d93a79544aa2c46f4d71643453085ac02
fromopac86 <fromopac@gmail.com>
2015-11-07 12:49:56 -0500

    Ejercicio

---
+++ refuerzo_modulo_6/lab31/www/js/lab1a.js
@@ -0,0 +1,9 @@
+angular.module('lab1a',  [])
+.controller('lab1aController', function($scope){
+        $scope.name = 'John F Rozo';
+        $scope.id = '16748805',
+        $scope.profession = "Ingeniero";
+        $scope.salary = 650000;
+        $scope.phone = 3106995544;
+        $scope.address = "Calle 9 No 111-33";
+});
